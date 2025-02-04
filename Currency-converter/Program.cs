c250a317b89d99d1cc4b6f64d36635ac82fc7b47
Jan Inge <jistrandman@gmail.com>
2025-02-04 14:37:54 +0100

    First commit

---
+++ Currency-converter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Currency_converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
